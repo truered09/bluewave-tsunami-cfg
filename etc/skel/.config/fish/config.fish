d8d10e6a5b80f836eab4c510a711f4ce3cb7acf5
TrueRed <64306333+RisingYT@users.noreply.github.com>
2022-12-10 08:37:52 +0800

    update

--- etc/skel/.config/fish/config.fish
+++ etc/skel/.config/fish/config.fish
@@ -1,4 +1,12 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-motivate
-end
+echo You might be wondering on why the terminal ran automatically.
+echo This is because we need to add the final touch.
+echo Now we shall update the packages and retrieve the repo databases from the internet.
+echo Do not worry. This is safe. If asked for a password then type it in. 
+echo Once this is done, the system will reboot once again.
+sudo pacman -Syyu --noconfirm
+cd ~/.config/fish
+cp config.fish.default config.fish
+rm config.fish.default
+cd ~/.config/autostart
+rm Alacritty.desktop
+reboot
