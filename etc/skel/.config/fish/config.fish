9421f180c323ecd20e6467c18bba56dccd0fe93b
TrueRed <64306333+RisingYT@users.noreply.github.com>
2022-12-18 07:42:24 +0800

    edit

--- etc/skel/.config/fish/config.fish
+++ etc/skel/.config/fish/config.fish
@@ -2,11 +2,11 @@ echo You might be wondering on why the terminal ran automatically.
 echo This is because we need to add the final touch.
 echo Now we shall update the packages and retrieve the repo databases from the internet.
 echo Do not worry. This is safe. If asked for a password then type it in. 
-echo Once this is done, the system will reboot once again.
-sudo pacman -Syyu --noconfirm
+sudo pacman -Syyu
 cd ~/.config/fish
 cp config.fish.default config.fish
 rm config.fish.default
 cd ~/.config/autostart
 rm Alacritty.desktop
-reboot
+echo All done. It is neccesarry but optional for you to reboot.
+killall alacritty
